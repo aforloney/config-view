0d49a0efb9d69e8c677ee778f1b37ff774a50642
Anthony Forloney <aforloney@gmail.com>
2022-01-03 01:06:41 -0500

    added more tests for complexity of objects

--- tests/ConfigViewerTests.cs
+++ tests/ConfigViewerTests.cs
@@ -22,7 +22,7 @@ namespace ConfigView.Tests
         public void When_NoConfiguration_Then_Empty()
         {
             // Arrange
-
+            // intetionally blank
             // Act
             var config = Subject.Get();
 
@@ -31,16 +31,15 @@ namespace ConfigView.Tests
         }
 
         [Fact]
-        public void When_JsonStreamConfigurationSupplied_Then_Contents_Returned()
+        public void When_JsonStreamSingleSimpleConfigurationSupplied_Then_Contents_Returned()
         {
             // Arrange
-            var jsonString = @"{""Key"":""Value2""}";
-            var rootConfig = FakeConfiguration.ConfigureRoot(jsonString);
-            Subject = new ConfigViewer(rootConfig);
+            var jsonString = @"{""Key"":""Value1""}";
+            Subject = new ConfigViewer(FakeConfiguration.ConfigureRoot(jsonString));
             var configView = new Config.ConfigView("Key", "Key", new Provider
                 {
                     Source = "JsonStreamConfigurationProvider",
-                    Value = "Value2"
+                    Value = "Value1"
                 });
             var expected = new List<Config.ConfigView> { configView };
 
@@ -49,6 +48,83 @@ namespace ConfigView.Tests
 
             // Assert
             expected.ShouldBeEquivalentTo(actual);
+        }
+
+        [Fact]
+        public void When_MultipleNestedConfigurationSupplied_Then_Contents_Returned()
+        {
+            // Arrange
+            var jsonString = @"{""KeyRoot"":{""InnerKey"":""Value1""}}";
+            Subject = new ConfigViewer(FakeConfiguration.ConfigureRoot(jsonString));
+            var configView = new Config.ConfigView("InnerKey", "KeyRoot:InnerKey", new Provider
+                {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value1"
+                });
+            var expected = new List<Config.ConfigView> { configView };
+
+            // Act
+            var actual = Subject.Get();
+
+            // Assert
+            expected.ShouldBeEquivalentTo(actual);
+        }
+
+        [Fact]
+        public void When_MultipleSimpleConfigurationsSupplied_Then_Contents_Returned()
+        {
+            // Arrange
+            var jsonString = @"{""KeyRoot"":{""InnerKey"":""Value1""},""OtherKey"":""Value2""}";
+            Subject = new ConfigViewer(FakeConfiguration.ConfigureRoot(jsonString));
+            var expected = new List<Config.ConfigView> {
+                new Config.ConfigView("InnerKey", "KeyRoot:InnerKey", new Provider
+                {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value1"
+                }),
+                new Config.ConfigView("OtherKey", "OtherKey", new Provider {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value2"
+                })
+             };
+
+            // Act
+            var actual = Subject.Get();
+
+            // Assert
+            expected.ShouldBeEquivalentTo(actual);
+        }
+
+        [Fact]
+        public void When_MultipleConfigurationsSuppliedMixed_Then_Contents_Returned()
+        {
+            // Arrange
+            var jsonString = @"{""Root"":""Value0"",""KeyRoot"":{""InnerKey"":""Value1""},""OtherKey"":""Value2""}";
+            Subject = new ConfigViewer(FakeConfiguration.ConfigureRoot(jsonString));
+            var expected = new List<Config.ConfigView> {
+                new Config.ConfigView("Root", "Root", new Provider
+                {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value0"
+                }),
+                new Config.ConfigView("InnerKey", "KeyRoot:InnerKey", new Provider
+                {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value1"
+                }),
+                new Config.ConfigView("OtherKey", "OtherKey", new Provider {
+                    Source = "JsonStreamConfigurationProvider",
+                    Value = "Value2"
+                })
+             };
+
+            // Act
+            var actual = Subject.Get();
+
+            // Assert
+            // Order of the keys matters so use a default sorting order for comparison
+            expected.OrderByDescending(e => e.Key)
+                .ShouldBeEquivalentTo(actual.OrderByDescending(act => act.Key));
         }
     }
 }
